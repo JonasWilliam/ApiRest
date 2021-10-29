d1dd9f4276a930e0b18c61a613f363deb5922ac6
JonasWilliam <jonaswilliamdelimasantos@gmail.com>
2021-10-28 22:39:05 -0300

    Crud Produto e Usuarios

---
+++ src/main/java/com/example/api/model/Produto.java
@@ -0,0 +1,76 @@
+package com.example.api.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+
+
+@Entity
+public class Produto implements Serializable{
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	@NotEmpty
+	private String nome;
+	//@NotEmpty
+	private double preco;
+	@NotEmpty
+	private String descricao;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public Produto(String nome, double preco, String descricao) {
+		this.nome = nome;
+		this.preco = preco;
+		this.descricao = descricao;
+	}
+	public Produto() {
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		return id == other.id;
+	}
+	
+	
+	
+}
